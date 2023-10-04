1e83ac54fa447eb98076a508c6ed4fddc163acf2
afsahurrehman11 <afsahurrehman11@gmail.com>
2023-10-04 09:50:38 +0500

    completed Playspace.h
    added following functions:
    place_stacks
    full_refresh_space
    hard_reset_space

--- afsah_and_hamza_solidate_project/afsah_and_hamza_solidate_project/Playspace.h
+++ afsah_and_hamza_solidate_project/afsah_and_hamza_solidate_project/Playspace.h
@@ -127,3 +127,84 @@ void Playspace::place_card(Card heldCard, int placeY, int placeX)
     }
 skip: {}
 }
+
+
+void Playspace::hard_reset_space(void)
+{
+    color_char tempChar;
+    tempChar.color_escape_code = black_c;
+    tempChar.hold_char = ' ';
+    for (int i = 1; i < height - 1; i++)
+    {
+        for (int j = 1; j < width - 1; j++)
+        {
+            playspace_image[i][j] = tempChar;
+        }
+    }
+    for (int i = 0; i < 7; i++)
+    {
+        if (i == 6)
+            this->place_card(emptySpaceFront, 1, 37);
+        this->place_card(emptySpaceFront, 1, 8 * i + 1);
+    }
+    for (int i = 0; i < 7; i++)
+    {
+        this->place_card(emptySpaceFront, 8, 8 * i + 1);
+    }
+
+    char stacklabels[7] = { '1', '2', '3', '4', '5', '6', '7' };
+    for (int i = 0; i < 7; i++)
+    {
+        tempChar.hold_char = stacklabels[i];
+        this->set_single_char(tempChar, 7, 8 * i + 3);
+    }
+
+}
+
+void Playspace::place_stacks(vector<FieldStack> stacks_container)
+{
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 1; j < stacks_container[i].cardsInStack.size(); j++)
+        {
+            this->place_card(stacks_container[i].cardsInStack[j], 2 * (j - 1) + 8, 1 + (8 * i));
+        }
+    }
+
+    if (stacks_container[draw_stock_index].cardsInStack.size() != 0)
+        this->place_card(stacks_container[draw_stock_index].cardsInStack.back(), 1, 49);
+
+    if (stacks_container[spade_foundation_index].cardsInStack.size() != 0)
+        this->place_card(stacks_container[spade_foundation_index].cardsInStack.back(), 1, 1);
+    if (stacks_container[heart_foundation_index].cardsInStack.size() != 0)
+        this->place_card(stacks_container[heart_foundation_index].cardsInStack.back(), 1, 9);
+    if (stacks_container[club_foundation_index].cardsInStack.size() != 0)
+        this->place_card(stacks_container[club_foundation_index].cardsInStack.back(), 1, 17);
+    if (stacks_container[diamond_foundation_index].cardsInStack.size() != 0)
+        this->place_card(stacks_container[diamond_foundation_index].cardsInStack.back(), 1, 25);
+
+    int talonSize = stacks_container[talon_pile_index].cardsInStack.size();
+    switch (talonSize)
+    {
+    case 0: break;
+    case 1: this->place_card(stacks_container[talon_pile_index].cardsInStack.back(), 1, 33);
+        break;
+    case 2: this->place_card(stacks_container[talon_pile_index].cardsInStack[talonSize - 2], 1, 33);
+        this->place_card(stacks_container[talon_pile_index].cardsInStack.back(), 1, 37);
+        break;
+    default: this->place_card(stacks_container[talon_pile_index].cardsInStack[talonSize - 3], 1, 33);
+        this->place_card(stacks_container[talon_pile_index].cardsInStack[talonSize - 2], 1, 37);
+        this->place_card(stacks_container[talon_pile_index].cardsInStack.back(), 1, 41);
+    }
+}
+
+void Playspace::full_refresh_space(vector<FieldStack> stacks_container)
+{
+    this->hard_reset_space();
+    this->place_stacks(stacks_container);
+    this->display_space();
+}
+
+
+
+
